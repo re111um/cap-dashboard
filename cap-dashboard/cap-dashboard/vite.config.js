799a1e2fcfc99c016fdd3eb96b1cc55cd386e632
re111um <qkrcksgud47@gmail.com>
2026-05-11 17:19:35 +0900

    Delete cap-dashboard directory

--- cap-dashboard/cap-dashboard/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/cap-dashboard/',  // GitHub repo 이름과 일치시켜야 함
-})
